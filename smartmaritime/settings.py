b808811a3b228142e1199d4ff779be023f9582f0
mkha <m_kha@hotmail.fr>
2025-07-04 18:55:25 +0200

    settings

--- smartmaritime/settings.py
+++ smartmaritime/settings.py
@@ -12,15 +12,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ucn*4#1nk*es8wq7lns+a!_)f@u_yb2-=tlg391myo=xn-lenq'
+# SECRET_KEY = 'django-insecure-ucn*4#1nk*es8wq7lns+a!_)f@u_yb2-=tlg391myo=xn-lenq'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
